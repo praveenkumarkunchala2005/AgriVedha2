5a16ed5b0e9df33798f6cbf29d62d0a91ccf593e
praveenkumarkunchala2005 <134261844+praveenkumarkunchala2005@users.noreply.github.com>
2024-01-11 15:55:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 from tensorflow.keras.preprocessing import image
 from urllib.parse import quote
 import joblib
-
+from googletrans import Translator
 
 # Load the trained models
 with open('crop_recommendation_model.pkl', 'rb') as model_file:
@@ -74,9 +74,16 @@ if page == "Disease Prediction":
     # Display the selected image
     if uploaded_file is not None:
         st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+    
+    languages = {"English": "en","Spanish": "es","French": "fr","Telugu": "te","Hindi": "hi","Tamil": "ta","Kannada": "kn","Malayalam": "ml"}
+    selected_language = st.selectbox("Select Language", list(languages.keys()), index=0)
+    if st.button("Select Language"):
+        language_code = languages[selected_language]
+        st.success(f"Selected Language: {selected_language}")
+        st.session_state.selected_language = language_code
 
     # Button to trigger disease prediction
-    if st.button("Predict Disease"):
+    if st.button("Predict Disease") and st.session_state.get("selected_language"):
         if uploaded_file is not None:
             # Make prediction for the uploaded image
             preds = model_predict(uploaded_file, model)
@@ -89,10 +96,65 @@ if page == "Disease Prediction":
             ]
             predicted_class = disease_class[np.argmax(preds[0])]
             confidence_score = np.max(preds[0])
+            result = predicted_class
+            cure = ""
+            prevention = ""
 
-            # Display results in a neat format
-            st.subheader("Prediction Results")
-            st.write(f"Predicted Disease: {predicted_class}")
+            if "Pepper__bell___Bacterial_spot" in result:
+                cure = "Cure: Apply copper-based fungicides or bactericides early in the disease cycle for bacterial spot on bell peppers."
+                prevention = "Prevention: Practice crop rotation, use resistant pepper varieties, and avoid overhead irrigation to minimize bacterial spot impact."
+            elif "Potato___Early_blight" in result:
+                cure = "Cure: Apply fungicides with chlorothalonil or copper to control early blight on potatoes."
+                prevention = "Prevention: Implement crop rotation, use disease-resistant potato varieties, and provide proper spacing to reduce early blight incidence."
+            elif "Potato___Late_blight" in result:
+                cure = "Cure: There is no cure for viral diseases. Management involves using virus-resistant tomato varieties and controlling whiteflies, which transmit the virus."
+                prevention = "Prevention: Implement measures to control whiteflies, such as using sticky traps and insecticides."
+            elif "Tomato_Bacterial_spot" in result:
+                cure = "Cure: Utilize fungicides containing chlorothalonil or copper to manage late blight on potatoes."
+                prevention = "Prevention: Practice good garden hygiene, employ crop rotation, and choose late blight-resistant potato varieties to minimize the risk of infection."
+            elif "Tomato_Early_blight" in result:
+                cure = "Cure: Use fungicides with chlorothalonil or copper to treat early blight on tomatoes."
+                prevention = "Prevention: Implement crop rotation, provide proper spacing for air circulation, and apply mulch to reduce soil splash and minimize early blight development."
+            elif "Tomato_Late_blight" in result:
+                cure = "Cure:  Apply fungicides containing chlorothalonil or copper to manage late blight on tomatoes."
+                prevention = "Prevention:  Practice good garden hygiene, implement crop rotation, and choose tomato varieties resistant to late blight to minimize the risk of infection."
+            elif "Tomato_Leaf_Mold" in result:
+                cure = "Cure: Use fungicides, such as those containing chlorothalonil, to manage leaf mold on tomatoes."
+                prevention = "Prevention: Ensure proper ventilation, avoid overhead watering, and select tomato varieties resistant to leaf mold to reduce the risk of infection."
+            elif "Tomato_Septoria_leaf_spot" in result:
+                cure = "Cure: Apply fungicides with copper or mancozeb to manage Septoria leaf spot on tomatoes."
+                prevention = "Prevention: Practice crop rotation, provide proper spacing for air circulation, and avoid overhead irrigation to minimize the occurrence of Septoria leaf spot."
+            elif "Tomato_Spider_mites_Two_spotted_spider_mite" in result:
+                cure = "Cure: Miticides can be used to control two-spotted spider mites on tomatoes."
+                prevention = "Prevention: Regularly monitor plants, promote natural enemies (predatory mites), and maintain proper humidity levels to discourage two-spotted spider mite infestations."
+            elif "Tomato__Target_Spot" in result:
+                cure = "Cure: Fungicides containing azoxystrobin or chlorothalonil can be used to manage target spot on tomatoes."
+                prevention = "Prevention: Implement crop rotation, maintain good garden hygiene, and choose tomato varieties resistant to target spot to minimize the risk of infection."
+            elif "Tomato__Tomato_YellowLeaf__Curl_Virus" in result:
+                cure = "Cure: Unfortunately, there is no cure for Tomato Yellow Leaf Curl Virus (TYLCV). Focus on using virus-resistant tomato varieties."
+                prevention = "Prevention: Implement rigorous whitefly control measures, employ physical barriers like insect nets, and practice good garden hygiene by removing and destroying infected plants promptly to prevent the spread of TYLCV."
+            elif "Tomato__Tomato_mosaic_virus" in result:
+                cure = "Cure: There is no cure for viral diseases. Management involves using virus-resistant tomato varieties and controlling whiteflies, which transmit the virus."
+                prevention = "Prevention: Implement measures to control whiteflies, such as using sticky traps and insecticides."
+            
+            a = "Prediction Results"
+            b = "Cure and Prevention Information"
+            language_code = st.session_state.selected_language
+            try:
+                translator = Translator()
+                translation1 = translator.translate(cure, dest=language_code)
+                translation2 = translator.translate(prevention, dest=language_code)
+                # Display the cure and prevention information
+                c = translator.translate(a, dest=language_code)
+                d = translator.translate(b, dest=language_code)
+
+                st.subheader(c.text)
+                st.subheader(d.text)
+                st.write(translation1.text)
+                st.write(translation2.text)
+            except Exception as e:
+                st.error(f"Translation error: {e}")
+                st.warning("Please try again or choose a different language.")
         else:
             st.warning("Please upload an image for disease prediction.")
 
@@ -117,3 +179,5 @@ elif page == "Crop Recommendation":
         st.write("The following crops are recommended based on the nutrient values you have provided.")
         st.write(f"Crop Name: {prediction1}")
         st.write(f"Fertilizer Name: {prediction2}")
+
+
